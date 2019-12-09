0172f81956ffcb6653e598fa81e16a7900925328
yutveg <kodi.0n31l@gmail.com>
2019-12-09 13:16:16 -0800

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from 'react'
+import { Route } from 'react-router-dom'
+import data from './data'
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from './components/Navigation'
+import Products from './components/Products'
+import ShoppingCart from './components/ShoppingCart'
+import { ProductContext } from './contexts/ProductContext.js'
+import { CartContext } from './contexts/CartContext.js'
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+    const [products] = useState(data)
+    const [cart, setCart] = useState([])
 
-	const addItem = item => {
-		// add the given item to the cart
-		setCart(cart.concat(item))
-	};
+    const addItem = item => {
+        // add the given item to the cart
+        setCart(cart.concat(item))
+    }
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    return (
+        <div className="App">
+            <ProductContext.Provider value={{ products, addItem }}>
+                <CartContext.Provider value={cart}>
+                    <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+                    {/* Routes */}
+                    <Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+                    <Route path="/cart" component={ShoppingCart} />
+                </CartContext.Provider>
+            </ProductContext.Provider>
+        </div>
+    )
 }
 
-export default App;
+export default App
